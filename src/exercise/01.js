7da71bf52ecdea4dc12653226253442f36d0b691
Cristian Perez <perez.cr@icloud.com>
2021-03-11 13:24:00 -0500

    Add notes to useState exercise.

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -1,29 +1,30 @@
 // useState: greeting
 // http://localhost:3000/isolated/exercise/01.js
 
-import * as React from 'react'
+import * as React from 'react';
 
-function Greeting() {
-  // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
+function Greeting({initialName = ''}) {
+  // Hey, React, this component can re-render any time this state that I want you to manage is going to change.
+  // React gives us the state value for the current render, and then it gives you a mechanism for updating that state, which is the state update or function. When you call that function then that will trigger the re-render.
+  const [name, setName] = React.useState(initialName);
 
   function handleChange(event) {
-    // 🐨 update the name here based on event.target.value
+    setName(event.target.value);
   }
 
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input onChange={handleChange} id="name" value={name} />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
-  )
+  );
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting initialName="Cristian" />;
 }
 
-export default App
+export default App;
